a1b34cecd6072a99b4527e8ddf37ce67a5d154d1
Vincent <vincent.osele@spherea.com>
2017-08-31 13:53:16 +0200

    gollum: restrict to textile format

---
+++ gollum/config.rb
@@ -0,0 +1,12 @@
+# always include this:
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+
+# first remove all markups:
+Gollum::Markup.formats.clear
+
+# and then define the sole markup to be supported:
+Gollum::Markup.formats[:textile] = {
+   :name => "Textile",
+   :regexp => /textile/
+}
+

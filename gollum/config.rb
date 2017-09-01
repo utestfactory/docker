6aebf78b6a6152b59293b8ba3eaccb4796d43f41
Vincent <vincent.osele@spherea.com>
2017-09-01 09:33:05 +0200

    gollum: use only config.rb

--- gollum/config.rb
+++ gollum/config.rb
@@ -1,12 +1,19 @@
-# always include this:
+# Remove const to avoid
+# warning: already initialized constant FORMAT_NAMES
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+# limit to one format
+Gollum::Page::FORMAT_NAMES = { :textile  => "Textile" }
 
-# first remove all markups:
-Gollum::Markup.formats.clear
+# Specify the path to the Wiki.
+gollum_path = '/wiki'
+Precious::App.set(:gollum_path, gollum_path)
 
-# and then define the sole markup to be supported:
-Gollum::Markup.formats[:textile] = {
-   :name => "Textile",
-   :regexp => /textile/
+# Specify the wiki options.
+wiki_options = {
+  :live_preview => true,
+  :allow_uploads => true,
+  :per_page_uploads => true,
+  :allow_editing => true
 }
+Precious::App.set(:wiki_options, wiki_options)
 
